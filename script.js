c7fe9d9a2207bc36c97c850f2dc501e4ec538616
Olga Mostepan <olgamostepan5@gmail.com>
2026-03-25 15:00:27 -0400

    Added script code

---
+++ script.js
@@ -0,0 +1,43 @@
+const buttons = document.querySelectorAll(".toggle-btn");
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const details = button.nextElementSibling;
+
+        details.classList.toggle("hidden");
+
+        button.textContent =
+            details.classList.contains("hidden")
+                ? "Show Details"
+                : "Hide Details";
+    });
+});
+
+
+const form = document.getElementById("contact-form");
+const errorMsg = document.getElementById("error-msg");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const message = document.getElementById("message").value.trim();
+
+    if (!name || !email || !message) {
+        errorMsg.textContent = "All fields are required.";
+        return;
+    }
+
+    if (!validateEmail(email)) {
+        errorMsg.textContent = "Invalid email.";
+        return;
+    }
+
+    errorMsg.textContent = "Form submitted!";
+    form.reset();
+});
+
+function validateEmail(email) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
